9e3dd831ecf72e4939debcf213481b320b34d588
tora_pan <travis.pandos@gmail.com>
2022-04-04 19:59:58 -0700

    finish boilerplate for react app without create-react-app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+const App = () => {
+  return <h1>This is hmmm gaga.</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
